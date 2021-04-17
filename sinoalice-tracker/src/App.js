f224fd12df952dadbd2ce184542da4828f9f91cb
unknown <klinton.jp@gmail.com>
2021-04-16 22:09:19 -0600

    added state

--- sinoalice-tracker/src/App.js
+++ sinoalice-tracker/src/App.js
@@ -1,35 +1,40 @@
 import './index.css';
 import { User } from './models/user.js';
+import { React, useState } from 'react';
 
 function App() {
-  let user = {
+  const [user, setUser] = useState({
     email : "",
     password : "",
     userName : ""
-  };
+    }
+  );
+
   let onSubmit = () => {
     console.log(user);
-  };
+  }
 
-  const handleChange = event => {
-    event.preventDefault();
-    console.log(event.target.value);
-  };
-
+  let handleChange = ({ target }) => {
+    setUser({ 
+      [target.email]: target.value,
+      [target.password]: target.value,
+      [target.userName]: target.value,
+     });
+  }
+  
   return (
     <div className="App">
         <h1>Login:</h1>
         <form>
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email" value={user.email} onChange={handleChange} /><br/>
-        <label for="password">Password:</label>
-        <input type="text" id="password" name="password" value={user.password} /><br/>
-        <label for="userName">User Name:</label>
-        <input type="text" id="userName" name="userName" value={user.userName} /><br/>
-        </form>
-        <button type="submit" onClick={onSubmit}>Log In</button>
+          <label for="email">Email:</label>
+          <input type="text" id="email" name="email" value={user.email} onChange={handleChange}/><br/>
+          <label for="password">Password:</label>
+          <input type="text" id="password" name="password" value={user.password} onChange={handleChange}/><br/>
+          <label for="userName">User Name:</label>
+          <input type="text" id="userName" name="userName" value={user.userName} onChange={handleChange}/><br/>
+        </form>        
+        <button type="submit" onClick= {onSubmit} >Log In</button>
     </div>
   );
 }
-
 export default App;
