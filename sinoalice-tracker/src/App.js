d794914bf9d58c9f45a53e94f462247e9e012e32
unknown <klinton.jp@gmail.com>
2021-04-16 21:46:24 -0600

    Updated form

--- sinoalice-tracker/src/App.js
+++ sinoalice-tracker/src/App.js
@@ -1,16 +1,33 @@
 import './index.css';
+import { User } from './models/user.js';
 
 function App() {
+  let user = {
+    email : "",
+    password : "",
+    userName : ""
+  };
+  let onSubmit = () => {
+    console.log(user);
+  };
+
+  const handleChange = event => {
+    event.preventDefault();
+    console.log(event.target.value);
+  };
+
   return (
     <div className="App">
         <h1>Login:</h1>
+        <form>
         <label for="email">Email:</label>
-        <input type="text" id="email" name="email" /><br/>
+        <input type="text" id="email" name="email" value={user.email} onChange={handleChange} /><br/>
         <label for="password">Password:</label>
-        <input type="text" id="password" name="password" /><br/>
+        <input type="text" id="password" name="password" value={user.password} /><br/>
         <label for="userName">User Name:</label>
-        <input type="text" id="userName" name="userName" /><br/>
-        <button type="submit">Log In</button>
+        <input type="text" id="userName" name="userName" value={user.userName} /><br/>
+        </form>
+        <button type="submit" onClick={onSubmit}>Log In</button>
     </div>
   );
 }
