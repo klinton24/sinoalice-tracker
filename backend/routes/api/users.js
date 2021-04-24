ff1c155858e7a3be0772ace4f1f49686654ffb63
unknown <klinton.jp@gmail.com>
2021-04-23 20:38:12 -0600

    backend working

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,23 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const keys = require("../../config/keys");
-
 const User = require("../../models/User");
 
 router.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  User.findOne({ email: req.body.email }).then((user) => {
-    if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      res.json({ success: false, message: "Well you screwed up." });
+      res.end();
     } else {
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      });
+      res.end();
     }
   });
+  // User.findOne({ email: req.body.email }).then((user) => {
+  //   if (user) {
+  //     return res.status(400).json({ email: "Email already exists" });
+  //   } else {
+  //     const newUser = new User({
+  //       name: req.body.name,
+  //       email: req.body.email,
+  //       password: req.body.password,
+  //     });
+  //   }
+  // });
 });
 
 module.exports = router;
